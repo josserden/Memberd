113715b6d5d3ff56e808217e5bc6f68df5247324
Den <52295780+josserden@users.noreply.github.com>
2023-05-03 17:21:12 +0300

    add animation to burger and add css counter

--- src/js/variables.js
+++ src/js/variables.js
@@ -3,5 +3,7 @@ export const getVariables = () => {
     body: document.querySelector('body'),
     burgerBtnOpen: document.querySelector('.js-burger-btn'),
     menuOverlay: document.querySelector('.js-menu-overlay'),
+    nextBtn: document.querySelector('.js-btn-next'),
+    prevBtn: document.querySelector('.js-btn-prev'),
   };
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { getVariables } from './js/variables.js';
 
-const { burgerBtnOpen, menuOverlay, body } = getVariables();
+const { burgerBtnOpen, menuOverlay, body, prevBtn, nextBtn } = getVariables();
 
 const toggleMenu = () => {
   const isMenuOpen = burgerBtnOpen.getAttribute('aria-expanded') === 'true';
@@ -15,3 +15,10 @@ const toggleMenu = () => {
 };
 
 burgerBtnOpen.addEventListener('click', toggleMenu);
+
+prevBtn.addEventListener('click', () => {
+  console.log('prev');
+});
+nextBtn.addEventListener('click', () => {
+  console.log('next');
+});
