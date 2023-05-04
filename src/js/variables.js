1093cdade6b86ce40318935af4ba019b70051f4a
Den <52295780+josserden@users.noreply.github.com>
2023-05-04 09:32:29 +0300

    add base script to active slider

--- src/js/variables.js
+++ src/js/variables.js
@@ -2,6 +2,8 @@ export const getVariables = () => {
   return {
     body: document.querySelector('body'),
     burgerBtnOpen: document.querySelector('.js-burger-btn'),
+    carousel: document.querySelector('.js-carousel'),
+    carouselItems: document.querySelectorAll('.js-carousel-item'),
     menuOverlay: document.querySelector('.js-menu-overlay'),
     nextBtn: document.querySelector('.js-btn-next'),
     prevBtn: document.querySelector('.js-btn-prev'),
--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import { getVariables } from './js/variables.js';
 
-const { burgerBtnOpen, menuOverlay, body, prevBtn, nextBtn } = getVariables();
+const {
+  burgerBtnOpen,
+  menuOverlay,
+  body,
+  prevBtn,
+  nextBtn,
+  carousel,
+  carouselItems,
+} = getVariables();
+
+let currentSlideIndex = 0;
+const lastSlideIndex = carouselItems.length - 1;
 
 const toggleMenu = () => {
   const isMenuOpen = burgerBtnOpen.getAttribute('aria-expanded') === 'true';
@@ -14,11 +25,30 @@ const toggleMenu = () => {
   }
 };
 
-burgerBtnOpen.addEventListener('click', toggleMenu);
+const goToSlide = index => {
+  if (index < 0 || index > lastSlideIndex) return;
 
-prevBtn.addEventListener('click', () => {
-  console.log('prev');
-});
-nextBtn.addEventListener('click', () => {
-  console.log('next');
-});
+  const scrollAmount =
+    carouselItems[index].offsetLeft - carouselItems[0].offsetLeft;
+
+  carousel.scrollTo({
+    left: scrollAmount,
+    behavior: 'smooth',
+  });
+};
+
+const handlePrevBtnClick = () => {
+  currentSlideIndex -= 1;
+  goToSlide(currentSlideIndex);
+};
+
+const handleNextBtnClick = () => {
+  if (currentSlideIndex === lastSlideIndex) return;
+
+  currentSlideIndex += 1;
+  goToSlide(currentSlideIndex + 1);
+};
+
+burgerBtnOpen.addEventListener('click', toggleMenu);
+prevBtn.addEventListener('click', handlePrevBtnClick);
+nextBtn.addEventListener('click', handleNextBtnClick);
