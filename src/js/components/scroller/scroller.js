a33ca9d994452624ff33e667e8ad35f998119aed
Den <52295780+josserden@users.noreply.github.com>
2023-06-02 15:40:45 +0300

    Finish task

--- src/js/components/scroller/scroller.js
+++ src/js/components/scroller/scroller.js
@@ -1,25 +1,37 @@
 export class Scroller extends HTMLElement {
   constructor() {
     super();
+    this.originalItems = [...this.children];
+    this.clonedItems = this.originalItems.map(item => {
+      const clone = item.cloneNode(true);
+      clone.classList.add('cloned');
+      return clone;
+    });
+    // Base variables for speed and position
     this.baseSizeInPx = 100;
     this.baseTimeInMs = 1000;
     this.speedValue =
       parseInt(this.getAttribute('speed-value')) || this.baseSizeInPx;
     this.speed = this.speedValue / this.baseTimeInMs;
     this.position = 0;
-
+    // Time variables for animation
     this.startTime = performance.now();
     this.pauseTime = null;
     this.isPlay = false;
-
+    // Observers options
+    this.intersectionOptions = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0,
+    };
+    // Observers
     this.resizeObserver = new ResizeObserver(this.handleResize.bind(this));
     this.observer = new IntersectionObserver(
       this.handleIntersection.bind(this),
-      {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0,
-      }
+      this.intersectionOptions
+    );
+    this.mutationObserver = new MutationObserver(
+      this.handleMutation.bind(this)
     );
   }
 
@@ -30,20 +42,56 @@ export class Scroller extends HTMLElement {
   connectedCallback() {
     if (!this.isConnected) return;
 
+    this.generateItems();
     this.startAnimation();
     this.addEventListener('mouseenter', this.pauseAnimation.bind(this));
     this.addEventListener('mouseleave', this.resumeAnimation.bind(this));
     this.resizeObserver.observe(this);
     this.observer.observe(this.parentElement);
+    this.mutationObserver.observe(this, {
+      childList: true,
+    });
+  }
+
+  generateItems() {
+    this.updateItems();
+    this.append(...this.clonedItems);
+  }
+
+  updateItems() {
+    // create a recursive function to update the items
+
+    // if the position is less than the height of the first item
+    if (this.position < this.originalItems[0].offsetHeight) {
+      // return the first item
+      return;
+    }
+
+    // otherwise, remove the first item
+    this.removeChild(this.originalItems[0]);
+
+    // and add it to the end of the list
+
+    this.appendChild(this.originalItems[0]);
+
+    // then update the position by subtracting the height of the first item
+    this.position -= this.originalItems[0].offsetHeight;
+
+    // then call the function again
+
+    this.updateItems();
   }
 
   disconnectedCallback() {
     this.resizeObserver.disconnect();
+    this.observer.disconnect();
+    // this.mutationObserver.disconnect();
   }
 
   startAnimation() {
-    this.isPlay = true;
     console.log('startAnimation');
+
+    this.isPlay = true;
 
     const init = () => {
       if (!this.isPlay) return;
@@ -53,7 +101,10 @@ export class Scroller extends HTMLElement {
         (this.pauseTime ? this.pauseTime : timestamp) - this.startTime;
       this.position = this.speed * elapsedTime;
 
-      this.style.transform = `translateY(${this.position}px)`;
+      this.updateItems();
+
+      this.style.transform = `translateY(${-this.position}px)`;
+
       requestAnimationFrame(init);
     };
 
@@ -61,14 +112,16 @@ export class Scroller extends HTMLElement {
   }
 
   pauseAnimation() {
+    console.log('pauseAnimation');
     this.isPlay = false;
     this.pauseTime = performance.now();
-    console.log('pauseAnimation');
+
+    this.mutationObserver.disconnect();
   }
 
   resumeAnimation() {
-    if (this.isPlay) return;
     console.log('resumeAnimation');
+    if (this.isPlay) return;
 
     this.isPlay = true;
     this.startTime += this.pauseTime ? performance.now() - this.pauseTime : 0;
@@ -83,8 +136,10 @@ export class Scroller extends HTMLElement {
         entry.target.getBoundingClientRect();
 
       if (width !== prevWidth || height !== prevHeight) {
+        console.log('handleResize');
+
         this.pauseAnimation();
-        this.startAnimation();
+        this.resumeAnimation();
       }
     }
   }
@@ -93,9 +148,17 @@ export class Scroller extends HTMLElement {
     for (const entry of entries) {
       if (entry.isIntersecting) {
         this.resumeAnimation();
+        console.log('handleIntersection');
         return;
       }
+
       this.pauseAnimation();
     }
+  }
+
+  handleMutation(mutations) {
+    console.log('handleMutation');
+    this.pauseAnimation();
+    this.resumeAnimation();
   }
 }
